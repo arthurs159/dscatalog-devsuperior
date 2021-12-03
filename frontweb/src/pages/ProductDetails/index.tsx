6fea94e8caed7706b823a844c6843bcd9ecf6d42
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-03 15:30:51 -0300

    useState useEffect use Params

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -2,23 +2,29 @@ import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId: string;
+}
+
 const ProductDetail = () => {
 
+  const { productId } = useParams<UrlParams>();
+  
   const [product, setProduct] = useState<Product>();
 
 
   useEffect( () => {
-    axios.get(BASE_URL + "/products/2")
+    axios.get(`${BASE_URL}/products/${productId}`)
     .then(response => {
       setProduct(response.data);
     });
-  }, []);
+  }, [productId]);
 
 
   return (

dcd2e15b33cf5cb219499e2922029511c7b3b56c
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-10 15:35:55 -0300

    Redirecting programmatically

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
@@ -19,6 +19,8 @@ const Login = () => {
 
   const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
+  const history = useHistory();
+
   const onSubmit = (formData : FormData) => {
     requestBackendLogin(formData)
     .then(response => {
@@ -27,6 +29,7 @@ const Login = () => {
       console.log('TOKEN GERADO: ' + token);
       setHasError(false);
       console.log('SUCESSO', response);
+      history.push('/admin')
     }).catch(error => {
       setHasError(true);
       console.log('ERROR', error);

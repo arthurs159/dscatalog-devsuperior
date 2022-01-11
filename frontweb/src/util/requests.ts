ffcd78a78c922885ca46babcc04f8b094ab266c1
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-11 16:07:02 -0300

    Add JWT decode and implementing

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
 
 type LoginResponse = {
   access_token: string;
@@ -68,21 +77,35 @@ export const getAuthData = () => {
 };
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(
+  function (config) {
     //
     return config;
-  }, function (error) {
+  },
+  function (error) {
     //
     return Promise.reject(error);
-  });
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  //
+axios.interceptors.response.use(
+  function (response) {
+    //
     return response;
-  }, function (error) {
+  },
+  function (error) {
     if (error.response.status === 401 || error.response.status === 403) {
       history.push('/admin/auth');
     }
     return Promise.reject(error);
-  });
+  }
+);
+
+export const getTokenData = (): TokenData | undefined => {
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
+};

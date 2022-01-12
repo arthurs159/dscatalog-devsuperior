cd84c78c19a69f0698153e13c93ba3bbf58bc00b
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-12 15:35:58 -0300

    Is authenticated method

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 import { ReactComponent as MainImage } from 'assets/images/main-img.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { getTokenData } from 'util/requests';
+import { getTokenData, isAuthenticated } from 'util/requests';
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
 
-    <h1>{getTokenData()?.user_name}</h1>
+    <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
 
       <div className="base-card home-card">
         <div className="home-content-container">
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -109,3 +109,8 @@ export const getTokenData = (): TokenData | undefined => {
     return undefined;
   }
 };
+
+export const isAuthenticated = () : boolean => {
+  const tokenData = getTokenData();
+  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}

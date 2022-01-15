0d8e89e0920d21759f24767888bbf35ffd46aa85
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-15 16:27:58 -0300

    Redirecting to intended route after login

--- frontweb/src/components/PrivateRoute/index.tsx
+++ frontweb/src/components/PrivateRoute/index.tsx
@@ -7,11 +7,15 @@ type Props = {
 };
 
 const PrivateRoute = ({ children, path }: Props) => {
+
   return (
     <Route
       path={path}
-      render={() =>
-        isAuthenticated() ? children : <Redirect to="/admin/auth/login" />
+      render={({location}) =>
+        isAuthenticated() ? children : <Redirect to={{
+          pathname: "/admin/auth/login",
+          state: { from: location }
+        }} />
       }
     />
   );
